c1157eb8c0a688ecfa501d65686ae73204a07a69
André Maldonado <andre.maldonado@gmail.com>
2018-03-27 11:23:13 -0300

    Codacy style improvements

--- src/config/server.js
+++ src/config/server.js
@@ -2,12 +2,12 @@ const PORT = 3394;
 var clientId = process.env.SLACK_CLIENTID;
 var clientSecret = process.env.SLACK_CLIENTSECRET;
 
-var express = require('express');
-var request = require('request');
+var express = require("express");
+var request = require("request");
 
 var app = express();
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 exports.app = app;
 exports.urlencodedParser = urlencodedParser;
@@ -16,16 +16,16 @@ app.listen(PORT, function () {
     console.log("habiticaslack listening on port " + PORT);
 });
 
-app.get('/oauth', function(req, res) {
+app.get("/oauth", function(req, res) {
     if (!req.query.code) {
         res.status(500);
         res.send({"Error": "Looks like we're not getting code."});
         console.log("Looks like we're not getting code.");
     } else {
         request({
-            url: 'https://slack.com/api/oauth.access', //URL to hit
-            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
+            url: "https://slack.com/api/oauth.access",
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
+            method: "GET",
 
         }, function (error, response, body) {
             if (error) {
@@ -34,11 +34,11 @@ app.get('/oauth', function(req, res) {
                 res.json(body);
 
             }
-        })
+        });
     }
 });
 
 app.use((err, request, response, next) => {
-  console.log(err)
-  response.status(500).send('Something broke!')
+  console.log(err);
+  response.status(500).send("Something broke!");
 });
