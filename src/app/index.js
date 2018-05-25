4257970ba09d438f296afe6dd90c213b3bf393d0
André Maldonado <andre.maldonado@gmail.com>
2018-05-25 17:52:34 -0300

    Tests implementation (#18)
    * Initial tests
    
    * Fix tests
    
    * Tests implementation

--- src/app/index.js
+++ src/app/index.js
@@ -1,26 +1,33 @@
 var server = require("../config/server");
 var request = require("request");
 var bodyParser = require("body-parser");
+var app = server.app;
 
-const GET_TASKS = "https://habitica.com/api/v3/tasks/user";
+const HABITICA_API = "https://habitica.com/api/v3"; //TODO: remove this url from here
+const GET_TASKS = "/tasks/user"; //TODO: remove this url from here
 
-server.app.post("/habitica", server.urlencodedParser, function(req, res) {
+app.post("/habitica", server.urlencodedParser, function(req, res) {
     if (typeof req.body !== "undefined" && req.body) {
         switch(req.body.text) {
             case "list":
                 request({
-                    url: GET_TASKS,
+                    url: HABITICA_API + GET_TASKS,
                     headers: { "x-api-user": process.env.HABITICA_USERID, "x-api-key": process.env.HABITICA_APITOKEN }
                 }, function (apiError, apiResponse, apiBody) {
-                    if (apiError) {
-                        res.send(apiError);
-                    } else {
+                    if (apiResponse.statusCode == 200) {
                         res.send(apiBody);
+                    } else {
+                        res.send("{\"success\":false, response:\"", apiResponse, "\"");
                     }
                 });
                 break; 
             default: 
-                res.send("still working on new tasks creation :(");
+                res.send({
+			"success": "false",
+			"message": "Still working on tasks creation"
+		});
         }
     }
 });
+
+module.exports = app;
---
+++ test/app.js
@@ -0,0 +1,51 @@
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var server = require("../src/app/index");
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+const HABITICA_API = "https://habitica.com/api/v3"; //TODO: remove this url from here
+var nock = require("nock");
+
+describe("/GET list", () => {
+
+    describe("when task list is requested", () => {
+        
+        var habiticaresponse = nock(HABITICA_API)
+            .get("/tasks/user")
+            .reply(200, 
+                {"success":true,"data":[{"_id":"8888461b-f5eb-4a16-97d3-c03380c422a3","userId":"b0413351-405f-416f-8787-947ec1c85199","text":"15 minute break","type":"reward","notes":"","tags":[],"value":10,"priority":1,"attribute":"str","challenge":{},"group":{"assignedUsers":[],"approval":{"required":false,"approved":false,"requested":false}},"reminders":[],"createdAt":"2017-01-07T17:52:09.121Z","updatedAt":"2017-01-11T14:25:32.504Z","id":"8a9d461b-f5eb-4a16-97d3-c03380c422a3"},,{"_id":"84c2e874-a8c9-4673-bd31-d97a1a42e9a3","userId":"b0413351-405f-416f-8787-947ec1c85199","alias":"prac31","text":"Practice Task 31","type":"daily","notes":"","tags":[],"value":1,"priority":1,"attribute":"str","challenge":{},"group":{"assignedUsers":[],"approval":{"required":false,"approved":false,"requested":false}},"reminders":[{"time":"2017-01-13T16:21:00.074Z","startDate":"2017-01-13T16:20:00.074Z","id":"b8b549c4-8d56-4e49-9b38-b4dcde9763b9"}],"createdAt":"2017-01-13T16:34:06.632Z","updatedAt":"2017-01-13T16:49:35.762Z","checklist":[],"collapseChecklist":false,"completed":true,"history":[],"streak":1,"repeat":{"su":false,"s":false,"f":true,"th":true,"w":true,"t":true,"m":true},"startDate":"2017-01-13T00:00:00.000Z","everyX":1,"frequency":"weekly","id":"84c2e874-a8c9-4673-bd31-d97a1a42e9a3"}],"notifications":[]}
+            );
+
+        it("it should return a list of user\'s tasks if everything is ok", (done) => {
+            chai.request(server)
+            .post("/habitica")
+            .type("urlencoded")
+            .send({text: "list"})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a("object");
+                res.should.have.property("text").to.match(/"success":true/);;
+                done();
+            });
+        });
+       
+        habiticaresponse = nock(HABITICA_API)
+            .get("/tasks/user")
+            .reply(401, "is no account that uses those credentials.");
+
+        it("it should return an error when wrong credentials are used", (done) => {
+            chai.request(server)
+            .post("/habitica")
+            .type("urlencoded")
+            .send({text: "list"})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.a("object");
+                res.should.have.property("text").to.match(/"success":false/);;
+                done();
+            });
+        });
+    });
+}); 
