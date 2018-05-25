fcd623a510962fb617e528aa79ba9166da6712a5
André Maldonado <andre.maldonado@gmail.com>
2018-05-25 17:40:19 -0300

    Tests implementation

--- src/app/index.js
+++ src/app/index.js
@@ -3,20 +3,21 @@ var request = require("request");
 var bodyParser = require("body-parser");
 var app = server.app;
 
-const GET_TASKS = "https://habitica.com/api/v3/tasks/user";
+const HABITICA_API = "https://habitica.com/api/v3"; //TODO: remove this url from here
+const GET_TASKS = "/tasks/user"; //TODO: remove this url from here
 
 app.post("/habitica", server.urlencodedParser, function(req, res) {
     if (typeof req.body !== "undefined" && req.body) {
         switch(req.body.text) {
             case "list":
                 request({
-                    url: GET_TASKS,
+                    url: HABITICA_API + GET_TASKS,
                     headers: { "x-api-user": process.env.HABITICA_USERID, "x-api-key": process.env.HABITICA_APITOKEN }
                 }, function (apiError, apiResponse, apiBody) {
-                    if (apiError) {
-                        res.send(apiError);
-                    } else {
+                    if (apiResponse.statusCode == 200) {
                         res.send(apiBody);
+                    } else {
+                        res.send("{\"success\":false, response:\"", apiResponse, "\"");
                     }
                 });
                 break; 
