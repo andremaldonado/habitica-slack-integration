18a5bc7a5f1655c6f4144b94d171bf3d59f2cc7d
André Maldonado <andre.maldonado@gmail.com>
2018-06-08 18:26:15 -0300

    List formatting (#19)
    * List of tasks finally formatted
    
    * Removing crappy files

--- src/config/server.js
+++ src/config/server.js
@@ -1,44 +1,27 @@
-const PORT = 3394;
+const PORT = 3394; //TODO: remove this from here and put it in a config file
 var clientId = process.env.SLACK_CLIENTID;
 var clientSecret = process.env.SLACK_CLIENTSECRET;
 
 var express = require("express");
 var request = require("request");
 
+var habitica = require("../app/habitica");
+
 var app = express();
 var bodyParser = require("body-parser");
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(urlencodedParser);
 
 app.listen(PORT, function () {
     console.log("habiticaslack listening on port " + PORT);
 });
 
-app.get("/oauth", function(req, res) {
-    if (!req.query.code) {
-        res.status(500);
-        res.send({"Error": "Looks like we're not getting code."});
-        console.log("Looks like we're not getting code.");
-    } else {
-        request({
-            url: "https://slack.com/api/oauth.access",
-            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
-            method: "GET",
-
-        }, function (error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                res.json(body);
-
-            }
-        });
-    }
-});
+app.route("/habitica").post(habitica.post);
 
 app.use((err, request, response, next) => {
   console.log(err);
   response.status(500).send("Something broke!");
 });
 
-exports.app = app;
-exports.urlencodedParser = urlencodedParser;
+module.exports = app;
